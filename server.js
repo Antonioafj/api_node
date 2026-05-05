96552ef8e125a8bc2267bb07376e8fc382f1c32f
Antonioafj <antonioafj.edu@gmail.com>
2026-05-05 13:33:47 -0400

    feat: create post and const users

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express"
+
+const app = express()
+app.use(express.json())
+
+app.post('/usuarios', (req, res) =>{
+    console.log(req.body)
+    res.send('Ok post')
+})
+
+app.get('/usuarios', (req, res) => {
+    res.send('Ok, deu bom')
+})
+
+app.listen(3000)
